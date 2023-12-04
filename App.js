ee1c7d655012cc0efd50cbbd69de548d49619c37
ShivaniAgarwal07 <shivaniagar7@gmail.com>
2023-12-04 13:01:34 +0530

    package-loch, gitignore, JSX starting code

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
-const parent= React.createElement("div", {id: "parent"},
- React.createElement("div", {id: "child"}, 
- [React.createElement("h1", {}, "I'm h1"),
- React.createElement("h2", {}, "I'm h2" )]
- )
-)
+const heading= <h1 id="heading">Namaste React using JSX</h1>;
+
+// const parent= React.createElement("div", {id: "parent"},
+//  React.createElement("div", {id: "child"}, 
+//  [React.createElement("h1", {}, "I'm h1"),
+//  React.createElement("h2", {}, "I'm h2" )]
+//  )
+// )
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local server
+- HMR- Hot Module Replacement
+- File Watching Algorithm
+- Caching- Faster Builds
+- Img Optimization
+- Minify
+- Tree shaking
+- HTTPs
+- Compression
+- Bundling
+- Consistent Hashing
+- Code Splitting
+- Differential building- supporting older browsers
+
+
